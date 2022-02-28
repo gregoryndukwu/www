2357b45d05279f41ddea088f4a2d2ee302b68a02
Gregory Ndukwu <ndukwusud@gmail.com>
2022-02-28 09:33:45 -0600

    te

---
+++ js/form.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var params = window.location.search.substring(1).split('&');
+    var size = params.length;
+    var userName = params[0].split("=",2);
+    var login = params[1].split("=",2);
+    var check = params[2].split("=",2);
+    console.log(size)
+
+    document.getElementById('login').innerHTML = userName[1];
+    document.getElementById('password').innerHTML = login[1];
+    
+    if (size > 3 ){
+        document.getElementById('checked').innerHTML = "Checked is On";
+    }else{
+        document.getElementById('checked').innerHTML = "Checked is Off";
+    }
+
+   
+
+}
